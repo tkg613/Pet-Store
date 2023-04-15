dbb2d7440961a04ec4b8511a341e2cf609cac345
tkg613 <lyntallow@mail.com>
2023-04-15 11:32:10 +0900

    Cleanup files

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,17 @@
 import {getFirestore} from 'firebase/firestore'
 import { initializeApp } from "firebase/app";
+require('dotenv').config()
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRY1D6tyRyqB-vBwXsEc39RDIPK83Tl7A",
-  authDomain: "pet-store-app-107d4.firebaseapp.com",
-  projectId: "pet-store-app-107d4",
-  storageBucket: "pet-store-app-107d4.appspot.com",
-  messagingSenderId: "77820951413",
-  appId: "1:77820951413:web:780ee368e6f85c2e74b13c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
